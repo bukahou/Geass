3fc302d8d28fd4af435b2583d94db5f6efda35b3
bukahou <zjh864781487@gmail.com>
2025-10-05 14:44:45 +0900

    新增dockerfile以及编译打包脚本

---
+++ build_and_push_history.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# 镜像名称与标签
+IMAGE_NAME="bukahou/geass-history"
+TAG="v1.0.0"
+
+echo "🔧 [Step 1] Checking Buildx builder"
+docker buildx create --name mybuilder --use || true
+docker buildx inspect --bootstrap
+
+echo "🚀 [Step 2] Building and pushing: ${IMAGE_NAME}:${TAG}"
+docker buildx build \
+  -f Dockerfile.history \
+  --platform linux/amd64,linux/arm64 \
+  -t ${IMAGE_NAME}:${TAG} \
+  --no-cache \
+  --push .
